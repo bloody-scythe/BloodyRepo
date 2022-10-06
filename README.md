0cfda27e8f2da161613625b6815aa1f4e08b12b2
Bloody-Scythe <bloodyscythe404@gmail.com>
2022-10-06 18:14:19 -0300

    Added repo database

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # BloodyRepo
 My arch repository
+
+to use add to pacman.conf:
+
+[bloody-repo]
+Server=https://github.com/bloody-scythe/BloodyRepo/$arch
--- buildrepo.py
+++ buildrepo.py
@@ -5,6 +5,7 @@ HERE = os.path.dirname(__file__)
 PKGDIR = os.path.join(HERE, "pkgbuild")
 PACKAGELIST = os.listdir(PKGDIR)
 REPO = os.path.join(HERE, 'x86_64')
+REPODB = os.path.join(REPO, 'bloody-repo.db.tar.gz')
 
 # Make packages and copy to repo
 for package in PACKAGELIST:
@@ -17,6 +18,8 @@ for package in PACKAGELIST:
         if '.tar.zst' in file:
             # Copy package file to repo
             newfile = shutil.copyfile(file, os.path.join(REPO,file))
+            # Add package to repo database
+            os.system('repo-add ' + REPODB + " " + newfile)
             # Add new package to git
             os.system('git add ' + newfile)
 
